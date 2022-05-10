180f538d942de3c02f813dfb45d796f60d33856c
Alexander Grant <agrant@dunelm.com>
2022-05-10 08:42:24 +0100

    Move wikipedia client to seperate module

--- src/my_hypermodern_python/console.py
+++ src/my_hypermodern_python/console.py
@@ -1,27 +1,14 @@
-
-from distutils.log import error
-import http
-from sys import api_version
 import textwrap
-from urllib.error import HTTPError
 
 import click
-import requests
 
-from . import __version__
-
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
-
-error_test = "https://httpbin.org/status/404"
+from . import __version__, wikipedia
 
 @click.command()
 @click.version_option(version=__version__)
 def main():
-    """This is a CLI tool to interact with an API."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
-        error = response
+    """The hypermodern Python project."""
+    data = wikipedia.random_page()
     title = data["title"]
     extract = data["extract"]
     click.secho(title, fg="green")
