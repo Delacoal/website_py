5b0df66eaa4fe76ebdc01764bbebdce82d64bc24
Alexander Grant <agrant@dunelm.com>
2022-05-16 07:35:00 +0100

    Add fixture with wikipedia.random_page mock

--- src/my_hypermodern_python/wikipedia.py
+++ src/my_hypermodern_python/wikipedia.py
@@ -1,13 +1,15 @@
 import requests
 import click
 
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 error_test = "https://httpbin.org/status/404"
 
-def random_page():
+def random_page(language="en"):
+    URL = API_URL.format(language=language)
+
     try:
-        with requests.get(API_URL) as response:
+        with requests.get(URL) as response:
             response.raise_for_status()
             return response.json()
     except requests.RequestException as error:
