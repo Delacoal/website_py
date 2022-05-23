9b4f10b40baabf517b80cb9f8f0b5fb42c612e36
Alexander Grant <alexander.grant@dunelm.com>
2022-05-23 08:22:54 +0100

    Exclude black from default Nox session

--- noxfile.py
+++ noxfile.py
@@ -2,17 +2,22 @@ import nox
 
 locations = "src", "tests", "noxfile.py"
 
+nox.options.sessions = "lint", "tests"
+
+
 @nox.session(python=["3.8"])
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov")
 
+
 @nox.session(python=["3.8"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8")
+    session.install("flake8", "flake8-black")
     session.run("flake8", *args)
+
 
 @nox.session(python="3.8")
 def black(session):
