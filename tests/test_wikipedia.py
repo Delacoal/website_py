c7555f8eadc7e6ea4ac58a6d0547c9ac332d531f
Alexander Grant <alexander.grant@dunelm.com>
2022-10-17 08:21:40 +0100

    Add test to check type of 'Page' dataclass

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,7 +1,13 @@
 from my_hypermodern_python import wikipedia
+from my_hypermodern_python.wikipedia import Page
 
 
 def test_random_page_uses_given_language(mock_requests_get):
     wikipedia.random_page(language="en")
     args, _ = mock_requests_get.call_args
     assert "en.wikipedia.org" in args[0]
+
+
+def test_random_page_returns_page(mock_requests_get):
+    page = wikipedia.random_page(language="en")
+    assert isinstance(Page, wikipedia.page)
