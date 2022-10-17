4578e7502a683c1846652fb870d983a6c58c40ee
Alexander Grant <alexander.grant@dunelm.com>
2022-10-17 08:11:17 +0100

    Refactor main using new dataclass 'Page'

--- src/my_hypermodern_python/console.py
+++ src/my_hypermodern_python/console.py
@@ -4,6 +4,8 @@ import click
 
 from . import __version__, wikipedia
 
+from src.my_hypermodern_python.wikipedia import Page
+
 
 @click.command()
 @click.option(
@@ -17,10 +19,7 @@ from . import __version__, wikipedia
 @click.version_option(version=__version__)
 def main(language: str) -> None:
     """The hypermodern Python project."""
-    data = wikipedia.random_page(language=language)
+    page = wikipedia.random_page(language=language)
 
-    title = data["title"]
-    extract = data["extract"]
-
-    click.secho(title, fg="green")
-    click.echo(textwrap.fill(extract))
+    click.secho(page.title, fg="green")
+    click.echo(textwrap.fill(page.extract))
